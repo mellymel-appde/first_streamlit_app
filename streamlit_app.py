f29db9b13b1f76f32448946c5246335d6ef5f05c
mellymel-appde <146849741+mellymel-appde@users.noreply.github.com>
2023-10-05 11:16:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu')
 streamlit.text('Idli')
 streamlit.text('Puri')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
