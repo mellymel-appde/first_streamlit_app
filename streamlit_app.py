f8759d478b4bc63c178452ad5b309ae6b0caf863
mellymel-appde <146849741+mellymel-appde@users.noreply.github.com>
2023-10-07 22:14:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 import snowflake.connector
 
